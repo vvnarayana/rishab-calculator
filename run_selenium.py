5fd2c9611c9d00b495d5ceca2e6209d5d7768a6c
Narayana <veera.v@trundl.com>
2024-05-02 05:09:16 +0530

    test new

--- run_selenium.py
+++ run_selenium.py
@@ -1,11 +1,12 @@
+import os
 import winrm # type: ignore
 
 username = 'Administrator'
 password = '$Voc8lIJ8L?g!1LylQTTZrWjHMVr)W1%'
 server_ip = '3.81.186.24'
-selenium_file_path = 'C:\\Users\\Administrator\\Downloads\\selenium_driver_code.py'
+selenium_file_path = r'C:\Users\Administrator\Downloads\selenium_driver_code.py'
 
-session = winrm.Session(server_ip, auth=(username, password))
+session = winrm.Session(server_ip, auth=(username, password), transport='ntlm')
 response = session.run_cmd('python', [selenium_file_path])
 
 print(response.std_out)

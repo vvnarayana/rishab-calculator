5c69017541903ced374e1f2634fc869e73d31016
Narayana <veera.v@trundl.com>
2024-05-02 16:09:15 +0530

    temp

--- run_selenium.py
+++ run_selenium.py
@@ -4,10 +4,20 @@ import winrm # type: ignore
 username = 'Administrator'
 password = '$Voc8lIJ8L?g!1LylQTTZrWjHMVr)W1%'
 server_ip = '3.81.186.24'
-selenium_file_path = r'C:\Users\Administrator\Downloads\selenium_driver_code.py'
+selenium_file_dir = r'C:\Users\Administrator\Downloads'
+selenium_file_name = 'selenium_driver_code.py'
 
 session = winrm.Session(server_ip, auth=(username, password), transport='ntlm')
-response = session.run_cmd('python', [selenium_file_path])
 
-print(response.std_out)
-print(response.std_err)
+# Construct the full file path
+selenium_file_path = os.path.join(selenium_file_dir, selenium_file_name)
+
+# Change the current working directory on the remote server
+response = session.run_cmd('cd', [selenium_file_dir])
+if response.status_code != 0:
+    print(f"Error changing directory: {response.std_err}")
+else:
+    # Execute the Python script
+    response = session.run_cmd('python', [selenium_file_name])
+    print(response.std_out)
+    print(response.std_err)
